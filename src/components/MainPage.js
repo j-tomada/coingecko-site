f0075b3ab515f67cfa0088924ec15a77d4eaf94c
jtomada <josephtomada@gmail.com>
2022-01-24 15:05:44 -0800

    Created useEffect hook which will fetch data from API and store into useState: exchanges.

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -2,6 +2,27 @@ import React, { useEffect, useState } from 'react';
 import Header from './Header'
 
 const MainPage = () => {
+    const [exchanges, setExchanges] = useState([]) //Exchanges stored here
+
+    /**
+     * useEffect runs once when MainPage Component is mounted
+     */
+    useEffect (() => {
+        //URL of first 10 exchanges
+        const url = 'https://api.coingecko.com/api/v3/exchanges?per_page=10';
+
+        //Fetch data from API and convert it to a JSON file
+        fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            //exchanges = data from the API
+            setExchanges(data)
+            console.log("API successfully fetched")
+        })
+    }, []);
+    //Successful if this logs 10 items
+    console.log(exchanges)
+
     return (
         <div>
             <Header/>
