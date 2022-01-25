99ba8101598a17abd638ac6ee3fa4f3d77df92d7
Joseph Tomada <josephtomada@gmail.com>
2022-01-25 01:11:46 -0800

    InfoPage is now centered to the middle of the page

--- src/components/InfoPage.js
+++ src/components/InfoPage.js
@@ -3,7 +3,6 @@ import './InfoPage.css';
 
 const InfoPage = ({ thisExchange }) => (
     <div className='background-container'>
-        <header></header>
         <div className='inner-container'>
 
         </div>
