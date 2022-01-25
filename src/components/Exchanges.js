15f987594c35974ce8c82dbb3b54a6a18f636af6
Joseph Tomada <josephtomada@gmail.com>
2022-01-25 00:18:11 -0800

    Fixed bug where certain were pathing to a string with a space, causing an invalid URL (swapped value.name for value.id).

--- src/components/Exchanges.js
+++ src/components/Exchanges.js
@@ -8,22 +8,24 @@ import MainPage from './MainPage';
 
 const Exchanges = ( {theExchange} ) => {
     return (
-        <Link to={theExchange.name} className='exchange-container'>
-            <header className='exchange-header'>
-                <img src={theExchange.image} width='75px' />
-                <h1 className='exchange-title'>{theExchange.name}</h1>
-            </header>
+        <Link to={theExchange.id} className=''>
+            <div className='exchange-container'>
+                <header className='exchange-header'>
+                    <img src={theExchange.image} width='75px' />
+                    <h1 className='exchange-title'>{theExchange.name}</h1>
+                </header>
 
-            <ul className='exchange-content'>
-                <div><b>Country: </b>{theExchange.country}</div>
-                <div><b>Trust Rank: </b>{theExchange.trust_score_rank}</div>
-                {/*URL Link*/}
-                <div>
-                    <b>URL: </b>
-                    <a href={theExchange.url}>{theExchange.url}</a>
-                </div>
-            </ul>
-            <img className='arrow' src={Arrow} />
+                <ul className='exchange-content'>
+                    <div><b>Country: </b>{theExchange.country}</div>
+                    <div><b>Trust Rank: </b>{theExchange.trust_score_rank}</div>
+                    {/*URL Link*/}
+                    <div>
+                        <b>URL: </b>
+                        <a href={theExchange.url}>{theExchange.url}</a>
+                    </div>
+                </ul>
+                <img className='arrow' src={Arrow} />
+            </div>
         </Link>
     );
 }
