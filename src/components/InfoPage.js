2fd61524461cf7d7282297456f08a9c5bae78c62
Joseph Tomada <josephtomada@gmail.com>
2022-01-25 01:16:35 -0800

    Adding information for the InfoPage component

--- src/components/InfoPage.js
+++ src/components/InfoPage.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './InfoPage.css';
 
-const InfoPage = ({ thisExchange }) => (
-    <div className='background-container'>
-        <div className='inner-container'>
+const InfoPage = ({ thisExchange }) => {
+    return (
+        <div className='background-container'>
+            <div className='inner-container'>
+                <div className='upper-container'>
+                    <img src={thisExchange.image} width='100px' />
+                    <ul className='content'>
+                        <li><b>Name: </b>{thisExchange.name}</li>
+                        <li><b>Country: </b>{thisExchange.country}</li>
+                        <li><b>Trust Rank: </b>{thisExchange.trust_score_rank}</li>
+                        <li><b>Year of Establishment: </b>{thisExchange.year_established}</li>
+                    </ul>
 
+                </div>
+
+            </div>
         </div>
-    </div>
-);
+    );
+}
 
 export default InfoPage;
