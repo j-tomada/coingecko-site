b751bbcd3db02427915788640360f6958ed54fd7
jtomada <josephtomada@gmail.com>
2022-01-24 16:27:46 -0800

    Created function b which bolds a certain word in a line

--- src/components/Exchanges.js
+++ src/components/Exchanges.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import './Exchanges.css'
 
-const Exchanges = ( {theExchange} ) => (
-    <button className='exchange-container'>
-        <header className='exchange-header'>
-            <img className='exchange-img' src={theExchange.image}/>
-            <h1 className='exchange-title'>{theExchange.name}</h1>
-        </header>
-        <div>{theExchange.country}</div>
-        <div>{theExchange.trust_score_rank}</div>
-        <div>{theExchange.url}</div>
-    </button>
-)
+const Exchanges = ( {theExchange} ) => {
+    /**
+     * Function used to bold certain parts of a line
+     * @param {*} props 
+     * @returns 
+     */
+    const b = (props) => (
+        <div style={{fontweight: 'bold'}}>{props.children}</div>
+    );
+
+    return (
+        <button className='exchange-container'>
+            <header className='exchange-header'>
+                <img className='exchange-img' src={theExchange.image}/>
+                <h1 className='exchange-title'>{theExchange.name}</h1>
+            </header>
+
+            <ul>
+                <div><b>Country: </b>{theExchange.country}</div>
+                <div><b>Trust Rank: </b>{theExchange.trust_score_rank}</div>
+                <div><b>URL: </b>{theExchange.url}</div>
+            </ul>
+        </button>
+    );
+}
 
 export default Exchanges
